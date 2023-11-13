c270d79ea4f95f25dcab84e679d75476ace4d9be
voidrak <112881704+voidrak@users.noreply.github.com>
2023-11-13 22:39:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="preview">123,745,854</div>
+        <div className="main-output">694,785,74,895</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>8</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
